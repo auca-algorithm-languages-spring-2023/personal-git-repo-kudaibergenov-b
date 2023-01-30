13c04a6f7832964af07be99f351bfc5f6a641a49
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2023-01-31 01:05:49 +0600

    Lab 1 Problem 1 Accepted

---
+++ lab-01/Problem1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    string text;
+    while (getline(cin, text))
+    {
+        list<char> beijuText;
+        auto it = beijuText.begin();
+
+        for (auto const ch : text)
+        {
+            if (ch == '[')
+            {
+                it = beijuText.begin();
+            }
+            else if (ch == ']')
+            {
+                it = beijuText.end();
+            }
+            else
+            {
+                beijuText.insert(it, ch);
+            }
+        }
+
+        for (auto const e : beijuText)
+        {
+            cout << e;
+        }
+
+        cout << '\n';
+    }
+}
