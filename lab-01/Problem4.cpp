f1e04a71fd8994d1ab76fabe9b89b9e2107d5942
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2023-02-02 00:57:48 +0600

    Lab 1 Problem 4 Accepted

---
+++ lab-01/Problem4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+   string command;
+   stack<string> names;
+   string t;
+   getline(cin, t);
+   int n = stoi(t);
+
+   while (n--)
+   {
+        getline(cin, command);
+
+        if (command == "Kick")
+        {
+            if (!names.empty())
+            {
+                names.pop();
+            }
+        }
+        else if (command == "Test")
+        {
+            if (!names.empty())
+            {
+                cout << names.top() << '\n';
+            }
+            else
+            {
+                cout << "Not in a dream" << '\n';
+            }
+        }
+        else
+        {
+            string name = command.substr(6);
+            names.push(name);
+        }
+   }
+}
