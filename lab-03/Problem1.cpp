557e4861d421020fafdb32e6598f18b36a5ae254
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2023-02-18 21:32:37 +0600

    Lab 3 Problem 1 Presentation error

--- lab-03/Problem1.cpp
+++ lab-03/Problem1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 multiset<int>::iterator cur;
 multiset<int> nums;
-int k, m, n;
+int k, m, n, index;
 vector<int> addVec(30001), getVec(30001);
 
 int main()
@@ -15,8 +15,9 @@ int main()
 
     while (k--)
     {
-        cout << '\n';
-        int index = 0;
+        index = 0;
+
+        cout << endl;
         cin >> m >> n;
 
         for (int i = 0; i < m; i++)
@@ -34,7 +35,7 @@ int main()
 
         for (int i = 1; i < m; i++)
         {
-            while (i == getVec[index])
+            while (i == getVec[index] && index < n)
             {
                 cout << *cur << endl;
                 ++cur;
@@ -42,13 +43,20 @@ int main()
             }
 
             nums.insert(addVec[i]);
+
+            if (cur == end(nums) || addVec[i] < *cur)
+            {
+                --cur;
+            }
         }
 
-        while (getVec[index] < m)
+        while (getVec[index] == m && index < n)
         {
             cout << *cur << endl;
             ++cur;
             ++index;
         }
+
+        nums.clear();
     }
 }
