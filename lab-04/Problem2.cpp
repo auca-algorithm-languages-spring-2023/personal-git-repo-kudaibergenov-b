2bf8728a59f1d0ade1b5057ecfafc2bb550f6bb1
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2023-02-25 20:30:01 +0600

    Lab 4 Problem 2 Wrong answer

---
+++ lab-04/Problem2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    for (;;)
+    {
+        int n;
+        cin >> n;
+
+        if (n == 0)
+        {
+            break;
+        }
+
+        multiset<int> bills;
+        int result = 0;
+
+        for (int day = 0; day < n; day++)
+        {
+            int k;
+            cin >> k;
+
+            for (int bill = 0; bill < k; bill++)
+            {
+                int value;
+                cin >> value;
+
+                bills.insert(value);
+            }
+
+            result += *--end(bills) - *begin(bills);
+            bills.erase(begin(bills));
+            bills.erase(--end(bills));
+        }
+
+        cout << result << endl;
+    }
+}
